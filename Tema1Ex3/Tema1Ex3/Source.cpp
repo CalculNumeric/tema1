41eededf5a9040fe4ba3a12fbbfcb83fd983b52a
citeaalexandru <citeaalexandru@gmail.com>
2015-03-03 23:37:54 +0200

    Non-functional framework - to implement the actual 4-russians algorithm

--- Tema1Ex3/Tema1Ex3/Source.cpp
+++ Tema1Ex3/Tema1Ex3/Source.cpp
@@ -1,4 +1,33 @@
 /*
 	--- THE FOUR RUSSIANS - BOOLEAN MATRIX MULTIPLICATION ---
+				- Implementation example -
 
+	This example uses a maximum o
 */
+#include "Matrix.h"
+#include <stdlib.h>
+#include <iostream>
+#define MatrixSizeW 5
+#define MatrixSizeH 5
+
+using namespace std;
+
+int main()
+{
+	int i;
+	BooleanMatrix A(MatrixSizeW, MatrixSizeH), B(MatrixSizeW, MatrixSizeH);
+	
+	// Initialize matrix
+	for (i = 1; i <= 5; i++)
+	{
+		A.setRowInt(i - 1, 0, i);
+		B.setRowInt(i - 1, 0, i + 2);
+	}
+
+	// Print matrixes
+	//cout << "Matrix A: \n" << A;
+	//cout << "Matrix B: \n" << B;
+	//cout << "Matrix C: \n" << A*B;
+
+	return 0;
+}
